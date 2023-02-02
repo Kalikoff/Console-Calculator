83e6a998d517272ebf1c1001e3e507fd7f4a7b59
Kalikoff <kalikoff.s@yandex.ru>
2023-02-02 11:37:07 +0300

    Add addition of numbers

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleCalculator {
+    class Program {
+        static void Main(string[] args) {
+            int a, b, c;
+
+            Console.WriteLine("Введите первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine($"Ответ сложение целых чисел: {c}");
+        }
+    }
+}
